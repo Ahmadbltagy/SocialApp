5fbd91c44bdd1e92e09955be551d2ec0b979189a
Ahmad Elbeltagy <elbeltagyahmad@gmail.com>
2023-05-05 12:16:29 +0300

    Initial Commit

---
+++ SocialApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SocialApp.API.Models;
+
+namespace SocialApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ SocialApp.API/Program.cs
@@ -0,0 +1,33 @@
+using SocialApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Connect to database
+builder.Services.AddDbContext<DataContext>(options 
+    =>options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.Run();
